3492ab8b44a64d3b542c2eb6930c707ada2a8b42
Caleb Lowhorn <celowhorn42@outlook.com>
2024-03-10 19:43:26 -0500

    Table info. fetch

---
+++ readFile.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+# Connect to SQLite database
+conn = sqlite3.connect('quiz_bowl.db')
+cursor = conn.cursor()
+
+
+
+# Function to retrieve table names
+def get_table_names():
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    table_names = cursor.fetchall()
+    return [name[0] for name in table_names]
+print("Table Names:",get_table_names(),"\n\n")
+
+# Function to retrieve all data from a table
+def get_table_data(table_name):
+    cursor.execute(f"SELECT * FROM {table_name}")
+    table_data = cursor.fetchall()
+    print(table_name,table_data)
+get_table_data("Business MGMT")
+get_table_data("DatabaseMGMT")
+get_table_data("Business Stats")
+get_table_data("Marketing")
+get_table_data("Business_App_Development")
